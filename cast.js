953d7ba8111379787ba346e1b34456963a70dbdd
Joyce <yh3048@tc.columbia.edu>
2019-11-15 15:18:37 -0500

    add cast&crew page without slider effect

---
+++ cast.js
@@ -0,0 +1,77 @@
+var myImg0 = document.getElementById('myimg0');
+var myImgJ = document.getElementById('myimgJ');
+var myImgC = document.getElementById('myimgC');
+var myImgL = document.getElementById('myimgL');
+var myImgJo = document.getElementById('myimgJo');
+var myImgCh = document.getElementById('myimgCh');
+var myImgR = document.getElementById('myimgR');
+
+function castImgJ(){
+  myImg0.style.opacity="0";
+  myImgC.style.opacity="0";
+  myImgL.style.opacity="0";
+  myImgJo.style.opacity="0";
+  myImgCh.style.opacity="0";
+  myImgR.style.opacity="0";
+  myImgJ.style.opacity="0.9";
+};
+
+function castImgC(){
+  myImg0.style.opacity="0";
+  myImgJ.style.opacity="0";
+  myImgL.style.opacity="0";
+  myImgJo.style.opacity="0";
+  myImgCh.style.opacity="0";
+  myImgR.style.opacity="0";
+  myImgC.style.opacity="0.9";
+}
+
+function castImgL(){
+  myImg0.style.opacity="0";
+  myImgJ.style.opacity="0";
+  myImgC.style.opacity="0";
+  myImgJo.style.opacity="0";
+  myImgCh.style.opacity="0";
+  myImgR.style.opacity="0";
+  myImgL.style.opacity="0.9";
+}
+
+function castImgJo(){
+  myImg0.style.opacity="0";
+  myImgJ.style.opacity="0";
+  myImgC.style.opacity="0";
+  myImgL.style.opacity="0";
+  myImgCh.style.opacity="0";
+  myImgR.style.opacity="0";
+  myImgJo.style.opacity="0.9";
+}
+
+function castImgCh(){
+  myImg0.style.opacity="0";
+  myImgJ.style.opacity="0";
+  myImgC.style.opacity="0";
+  myImgL.style.opacity="0";
+  myImgJo.style.opacity="0";
+  myImgR.style.opacity="0";
+  myImgCh.style.opacity="0.9";
+}
+
+function castImgR(){
+  myImg0.style.opacity="0";
+  myImgJ.style.opacity="0";
+  myImgC.style.opacity="0";
+  myImgL.style.opacity="0";
+  myImgJo.style.opacity="0";
+  myimgCh.style.opacity="0";
+  myImgR.style.opacity="0.9";
+}
+
+function castImg0(){
+  myImgR.style.opacity="0";
+  myImgJ.style.opacity="0";
+  myImgC.style.opacity="0";
+  myImgL.style.opacity="0";
+  myImgJo.style.opacity="0";
+  myimgCh.style.opacity="0";
+  myImg0.style.opacity="0.9";
+}
