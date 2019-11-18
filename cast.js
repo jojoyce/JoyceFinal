15a4dcb2015cf2260e7852b946040d783fbe1aae
Joyce <yh3048@tc.columbia.edu>
2019-11-18 15:22:24 -0500

    cast&crew slider finished/gallery begins

--- cast.js
+++ cast.js
@@ -75,3 +75,40 @@ function castImg0(){
   myimgCh.style.opacity="0";
   myImg0.style.opacity="0.9";
 }
+
+
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  const current = document.querySelector('.current');
+  current.classList.remove('current');
+  if (current.nextElementSibling) {
+  current.nextElementSibling.classList.add('current');
+} else {
+  slides[0].classList.add('current');
+}
+  setTimeout(() => current.classList.remove('current'));
+}
+
+const prevSlide = () => {
+  const current = document.querySelector('.current');
+  current.classList.remove('current');
+  if (current.previousElementSibling) {
+  current.previousElementSibling.classList.add('current');
+} else {
+  slides[slides.length - 1].classList.add('current');
+}
+  setTimeout(() => current.classList.remove('current'));
+}
+
+next.addEventListener('click', e =>{
+  nextSlide();
+});
+prev.addEventListener('click', e =>{
+  prevSlide();
+});
